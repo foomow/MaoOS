665612ae5f8b2b3f883803337f6a5632ed1e0bbe
Maodashu <foomow@163.com>
2020-06-11 17:36:20 +0800

    update

--- src/c/i386/elf.c
+++ src/c/i386/elf.c
@@ -1,18 +1,38 @@
-void get_elf_header(char *filename)
+void get_elf_header(char* filename)
 {
 	prints(filename); print_cr();
 	Elf32_Ehdr elf_header;
-	uint32 readcount = readfile(filename, 0, &elf_header,sizeof(elf_header));
+	uint32 readcount = readfile(filename, 0, (char*)&elf_header, sizeof(elf_header));
 	print_word_hex(readcount);; print_cr();
 
-	//printf("section headers table addr:0x%X\n",elf_header.e_shoff);
-	//printf("section count:0x%X\n",elf_header.e_shnum);
-	//printf("section name string index:0x%X\n",elf_header.e_shstrndx);
 	prints("section headers table addr:");
 	print_word_hex(elf_header.e_shoff);; print_cr();
 	prints("section count:");
 	print_word_hex(elf_header.e_shnum);; print_cr();
 	prints("section name string index:");
 	print_word_hex(elf_header.e_shstrndx);; print_cr();
-	
+
+	uint16 section_code_idx = 0xffff;
+	uint16 section_rel_idx = 0xffff;
+	uint16 section_sym_idx = 0xffff;
+	uint16 section_symname_idx = 0xffff;
+	uint16 section_bss_idx = 0xffff;
+	uint16 section_rodata_idx = 0xffff;
+	uint16 section_data_idx = 0xffff;
+
+	Elf32_Shdr section;
+	uint16 off = elf_header.e_shoff;
+	for (int i=0;i< elf_header.e_shnum;i++)
+	{
+		readcount = readfile(filename, off,(char*)&section, sizeof(Elf32_Shdr));
+		prints("section id:");
+		print_dword_hex(i);; print_cr();
+		prints("section off:");
+		print_dword_hex(section.sh_offset);; print_cr();
+		prints("section size:");
+		print_dword_hex(section.sh_size);; print_cr();
+		prints("=================");; print_cr();
+		off = off + readcount;
+	}
+
 }
